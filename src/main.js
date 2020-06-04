defd8419051e12731a137036599ecf3609ac966d
demkantor <demkantor@gmail.com>
2020-06-03 21:46:42 -0500

    can now delete and edit events, can create new events

--- src/main.js
+++ src/main.js
@@ -24,6 +24,17 @@ firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 
+
+// const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.';
+// Vue.config.warnHandler = function (msg, vm, trace) {
+//   // `trace` is the component hierarchy trace
+//   if (msg === ignoreWarnMessage) {
+//     msg = null;
+//     vm = null;
+//     trace = null;
+//   }
+// }
+
 new Vue({
   vuetify,
   render: h => h(App)
